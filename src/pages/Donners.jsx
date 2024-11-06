2acd999d5e9782f1a37f410cff5b65f9bd15293e
Md Atikuzzaman <akash203037@gmail.com>
2024-11-07 00:35:46 +0600

    queary paramiter need to added

--- src/pages/Donners.jsx
+++ src/pages/Donners.jsx
@@ -7,14 +7,17 @@ const Donners = () => {
   const [donners, setDonners] = useState([]);
   const [villages, setVillages] = useState([]);
 
-  const [selectedVillage, setSelecetedVillage] = useState("");
-  const [selectedBloodGroup, setSelecetedBloodGroup] = useState("");
+  const [selectedVillage, setSelectedVillage] = useState("");
+  console.log(selectedVillage);
+  const [selectedBloodGroup, setSelectedBloodGroup] = useState("");
+  console.log(selectedBloodGroup);
 
   const fetchVillage = () => {
     axios(`${import.meta.env.VITE_BASE_URL}/villages`)
       .then((res) => setVillages(res.data))
       .catch((err) => console.error(err));
   };
+
   useEffect(() => {
     fetchVillage();
     axios(`${import.meta.env.VITE_BASE_URL}/donner's`)
@@ -24,34 +27,45 @@ const Donners = () => {
       .catch((err) => console.error(err));
   }, []);
   // console.log(donners);
-  console.log(villages);
+  // console.log(villages);
   return (
     <div className="overflow-x-auto">
-      <div className="grid md:grid-cols-2 items-center mb-8">
-        <div>
-          <form action="" className="bg-gray-100 w-[90%] mx-auto rounded-md">
+      <div className="bg-gray-100 p-4 grid md:grid-cols-2 items-center gap-4 mb-8">
+        <>
+          <form
+            action=""
+            className="bg-white w-[90%] mx-auto rounded-md flex items-center"
+          >
             <input
               type="text"
               placeholder="ডোনার এর নাম দিয়ে সার্চ করুন"
               className="w-[90%] p-4 bg-transparent outline-none border-none"
             />
-            <button type="submit" className="w-[10%] btn">
-              <FaSearch className="mx-auto text-red-400 text-xl" />
+            <button type="submit" className="w-[10%] border-l-2" title="search">
+              <FaSearch className="mx-auto text-red-400 text-2xl" />
             </button>
           </form>
-        </div>
+        </>
         <div className="flex justify-between items-center">
           <h2 className="font-semibold text-lg">ডোনার সর্ট করুন </h2>
-          <select className="border border-red-400 text-gray-600 font-semibold p-2 rounded-md outline-none">
-            <option defaultValue="">গ্রাম এর নাম</option>
+          <select
+            value={selectedVillage}
+            onChange={(e) => setSelectedVillage(e.target.value)}
+            className="border border-red-400 text-gray-600 font-semibold p-2 rounded-md outline-none"
+          >
+            <option defaultValue="">--গ্রাম এর নাম--</option>
             {villages.map((village, i) => (
               <option key={i} value={village}>
                 {village}
               </option>
             ))}
           </select>
-          <select className="border border-red-400 text-gray-600 font-semibold p-2 rounded-md  outline-none">
-            <option defaultValue="">ব্লাড গ্রুপ</option>
+          <select
+            value={selectedBloodGroup}
+            onChange={(e) => setSelectedBloodGroup(e.target.value)}
+            className="border border-red-400 text-gray-600 font-semibold p-2 rounded-md  outline-none"
+          >
+            <option defaultValue="">--ব্লাড গ্রুপ--</option>
             <option value="A+">A+</option>
             <option value="A-">A-</option>
             <option value="B+">B+</option>
