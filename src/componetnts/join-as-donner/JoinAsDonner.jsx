a83b634b90a44b81f72a6438aba824caee7c4875
Md Atikuzzaman <akash203037@gmail.com>
2024-12-07 12:49:51 +0600

    color modified

--- src/componetnts/join-as-donner/JoinAsDonner.jsx
+++ src/componetnts/join-as-donner/JoinAsDonner.jsx
@@ -8,10 +8,10 @@ const JoinAsDonner = () => {
         return
     }
     return (
-            <div className='flex flex-col-reverse md:flex-row items-center justify-center py-4 bg-red-100 mt-8 rounded-t-3xl rounded-b-lg'>
+            <div className='flex flex-col-reverse md:flex-row items-center justify-center  gap-6 py-4 bg-red-100 mt-8 rounded-t-3xl rounded-b-lg'>
             <div className='px-2'>
-                <h2 className='text-4xl md:text-6xl lg:text-8xl font-bold mb-6 uppercase'>Join as <span className='text-red-600'>a Donner</span></h2>
-                <Link className='w-full bg-red-600 px-4 py-2 rounded-md inline-block text-center font-semibold text-white text-xl' to={"/register"}>JOIN NOW</Link>
+                <h2 className='text-4xl md:text-6xl lg:text-8xl font-bold mb-6 uppercase'>Join as <span className='text-red-500'>a Donner</span></h2>
+                <Link className='w-full bg-red-500 px-4 py-2 rounded-md inline-block text-center font-semibold text-white text-xl' to={"/register"}>JOIN NOW</Link>
             </div>
             <div><img src="/donner_hero.png" alt="donner" /></div>
             </div>
